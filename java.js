da3a715e242185d8a41e8d2e7f3b6f1d755287df
ArrozKunProject <davidnoobgamer@gmail.com>
2025-12-29 18:03:28 -0400

    Se agrego

---
+++ java.js
@@ -0,0 +1,12 @@
+document.getElementById("click").addEventListener("click", function(){
+    document.getElementById("a").style.backgroundColor = "white";
+    if (document.getElementById("a").style.backgroundColor = "white") {
+
+    document.getElementById("a").style.backgroundColor = "blue";  
+    }
+    else {
+        
+    document.getElementById("a").style.backgroundColor = "white";
+    }
+    
+});
